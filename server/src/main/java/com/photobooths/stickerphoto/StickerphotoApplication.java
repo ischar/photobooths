b427c398a36d85fb934dffe2a86ba8a4d3500d3d
Jaeho Cha <57610574+ischar@users.noreply.github.com>
2023-07-16 14:41:34 +0900

    Initial commit

---
+++ server/src/main/java/com/photobooths/stickerphoto/StickerphotoApplication.java
@@ -0,0 +1,13 @@
+package com.photobooths.stickerphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickerphotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickerphotoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/photobooths/stickerphoto/StickerphotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photobooths.stickerphoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickerphotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
